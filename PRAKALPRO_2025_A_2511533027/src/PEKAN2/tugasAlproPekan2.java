06f86ea5c9a772c0323061cb40e132b978e44ec2
hp <hp@LAPTOP-MGK4V63A>
2025-09-21 20:10:00 +0700

    tugas prakalpro pekan 2

---
+++ PRAKALPRO_2025_A_2511533027/src/PEKAN2/tugasAlproPekan2.java
@@ -0,0 +1,19 @@
+package PEKAN2;
+
+public class tugasAlproPekan2 {
+	public static void main(String[] args) {
+		// Data eksperimen sains 
+		int jumlahSampel = 50;
+		float suhuRataRata = 27.8f;
+		char tingkatAkurasi = 'A';
+		boolean dataValid = true;
+		
+		// Output ke layar
+		System.out.println("=== Data Eksperimen Sains ===");
+		System.out.println("Jumlah Sampel   : " + jumlahSampel);
+		System.out.println("Suhu Rata-Rata  : " + suhuRataRata);
+		System.out.println("Tingkat Akurasi : " + tingkatAkurasi);
+		System.out.println("Data Valid      : " + dataValid);
+	}
+
+}
