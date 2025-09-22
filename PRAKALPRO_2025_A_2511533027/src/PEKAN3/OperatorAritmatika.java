362473ffaf3b813241f60c65390475b08dcdc074
hp <hp@LAPTOP-MGK4V63A>
2025-09-22 14:27:17 +0700

    PEKAN 3 Praktikum Kelas A

---
+++ PRAKALPRO_2025_A_2511533027/src/PEKAN3/OperatorAritmatika.java
@@ -0,0 +1,33 @@
+package PEKAN3;
+
+import java.util.Scanner;
+
+public class OperatorAritmatika {
+	public static void main (String[] args) {
+		int A1;
+		int A2;
+		int hasil;
+		Scanner keyboard = new Scanner (System.in);
+		System.out.println("Input angka-1: ");
+		A1 = keyboard.nextInt();
+		System.out.println("Input angka-2: ");
+		A2 = keyboard.nextInt();
+		keyboard.close();
+		System.out.println("operator Penjumlahan");
+		hasil = A1 + A2;// penjumlahan
+		System.out.println("Hasil = " + hasil);
+		System.out.println("operator Pengurangan");
+		hasil = A1 - A2;// pengurangan
+		System.out.println("Hasil = " + hasil);
+		System.out.println("operator Perkalian");
+		hasil = A1 * A2;// perkalian
+		System.out.println("Hasil = " + hasil);
+		System.out.println("operator hasil bagi");
+		hasil = A1 / A2;// pembagian
+		System.out.println("Hasil = " + hasil);
+		System.out.println("operator sisa bagi");
+		hasil = A1 % A2;// Sisa bagi
+		System.out.println("Hasil = " + hasil);	
+	}
+
+}
