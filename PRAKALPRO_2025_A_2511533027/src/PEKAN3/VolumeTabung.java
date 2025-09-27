91495c6e266fa282e2528178adf58a503e44f8c3
hp <hp@LAPTOP-MGK4V63A>
2025-09-27 18:13:57 +0700

    Tugas Prakalpro Pekan 3

--- PRAKALPRO_2025_A_2511533027/src/PEKAN3/VolumeTabung.java
+++ PRAKALPRO_2025_A_2511533027/src/PEKAN3/VolumeTabung.java
@@ -1,5 +1,25 @@
 package PEKAN3;
 
+import java.util.Scanner;
+
 public class VolumeTabung {
+	public static void main(String [] args) {
+		double r;
+		double t;
+		double volume;
+		Scanner keyboard = new Scanner (System.in);
+		
+		// Input 
+		System.out.println("Input Jari-Jari Tabung: ");
+		r = keyboard.nextDouble();
+		System.out.println("Input Tinggi Tabung: ");
+		t = keyboard.nextDouble();
+		keyboard.close();
+		
+		// Proses menghitung volume tabung
+		System.out.println("=== Perhitungan Volume Tabung ===");
+		volume = 3.14 * r * r * t; // rumus volume tabung
+		System.out.println("Hasil Volume Tabung = " + volume);
+	}
 
 }
