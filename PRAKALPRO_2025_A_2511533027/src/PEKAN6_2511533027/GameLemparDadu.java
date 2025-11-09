3d5f5cf8a00351aac8d5f12ff37a051392697148
hp <hp@10.44.8.205>
2025-11-09 20:38:31 +0700

    prakalpro pekan 6

---
+++ PRAKALPRO_2025_A_2511533027/src/PEKAN6_2511533027/GameLemparDadu.java
@@ -0,0 +1,44 @@
+package PEKAN6_2511533027;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class GameLemparDadu {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		Random rand = new Random();
+		
+		int dadu1, dadu2, hasil;
+		int percobaan = 0;
+		String jawab;
+		boolean menang = false;
+		
+		System.out.println("=== Game Lempar Dadu ===");
+		do {
+			System.out.print("Apakah mau lempar dadu (ya / tidak?) ");
+			jawab = input.next();
+			if (jawab.equalsIgnoreCase("tidak")) {
+				System.out.println("\nPermainan dihentikan oleh pengguna. ");
+				break;
+			}
+			dadu1 = rand.nextInt(6) + 1; // 1 - 6
+			dadu2 = rand.nextInt(6) + 1;
+			hasil = dadu1 + dadu2;
+			percobaan++;
+			System.out.println(dadu1 + " + " + dadu2 + " = " + hasil);
+			if (hasil == 7) {
+				System.out.println("\nTebakan Anda Benar!");
+				System.out.println("Anda Menang Setelah " + percobaan + "Percobaan!");
+				menang = true;
+			} else {
+				System.out.println("Tebakan Anda Salah\n");
+			}
+			
+		} while (!menang);
+		if (!menang) {
+			System.out.println("Permainan Selesai. Anda Belum Mendapatkan Angka 7.");
+		}
+		input.close();
+	}
+}
